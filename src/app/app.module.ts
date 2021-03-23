c5456eaa31b877f297be4d15c95eb0c5df1f8433
Angular101010 <80366136+Angular101010@users.noreply.github.com>
2021-03-23 14:33:15 -0600

    Added  first Table/PC. Fixed Ang-Material and ng serve bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { InventoryComponent } from './inventory/inventory.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { InventoryDetailComponent } from './inventory-detail/inventory-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataTableComponent } from './data-table/data-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
@@ -14,13 +19,18 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     AppComponent,
     InventoryComponent,
     InventoryDetailComponent,
-    DashboardComponent
+    DashboardComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
