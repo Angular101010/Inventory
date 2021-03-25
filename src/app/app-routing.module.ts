09bd255a42973a5151718b96c92d6e14060bd406
Angular101010 <80366136+Angular101010@users.noreply.github.com>
2021-03-25 11:25:27 -0600

    Routed and Finished PC table. Routed list to Misc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InventoryComponent } from './inventory/inventory.component';
 import {DashboardComponent} from './dashboard/dashboard.component'
+import { DataTableComponent } from './data-table/data-table.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   {path: 'inventories', component: InventoryComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'data-table', component: DataTableComponent}
   
 ]
 
